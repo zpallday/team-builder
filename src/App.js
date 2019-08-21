69efce18e5dd94c56e1fa742a91e9b3403982caf
zachary-peasley <zpman95@hotmail.com>
2019-08-21 14:51:55 -0500

    Errors on app, building the form page next

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const  Team = [
+  {name:"Zachary Peasley", email:"zpman95@hotmail.com", role:"Programmer"},
+  {name:"Dave Smith", email:"Dave@google.com", role:"Doctor"},
+  {name:"Mary Loius", email:"Mary@hotmail.com", role:"Nurse"},
+]
+
+const App = () => {
+  const [teams, setTeams] = useState(Team)
+  const removeTeamMembers = (overboardMem) => {
+    const newTeam = team.filter(member => {
+    if (member.email !== overboardMem.email){
+      return member;
+    }
+  });
+  updateTeam(newTeam)
 }
+  return (
+    
 
+    <div className = " App ">
+      <Form addTeamMember = {updateTeam} team={team} />
+      {team.map((member) => <div className = "member-card">
+        <h1>Name: {member.name} </h1>
+        <p>Email: {member.email}</p>
+        <p>Role: {member.role} </p>
+        <button onClick={() => removeTeamMembers(member)}>
+          Man Down </button>        
+        
+        </div>)}
+
+
+
+    </div>
+  )
+}
 export default App;
